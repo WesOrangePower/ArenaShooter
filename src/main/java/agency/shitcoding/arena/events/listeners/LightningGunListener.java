499e849a1ac100aa7b31d8947655645de79b269f
Martin Borissov <admin@tooizi.eu>
2024-07-15 01:30:33 +0300

    Replaced GuardianBeam dependency with Paper API calls to support 1.21.

--- src/main/java/agency/shitcoding/arena/events/listeners/LightningGunListener.java
+++ src/main/java/agency/shitcoding/arena/events/listeners/LightningGunListener.java
@@ -6,10 +6,11 @@ import agency.shitcoding.arena.SoundConstants;
 import agency.shitcoding.arena.events.AmmoUpdateEvent;
 import agency.shitcoding.arena.events.GameDamageEvent;
 import agency.shitcoding.arena.events.GameShootEvent;
-import agency.shitcoding.arena.gamestate.Laser.GuardianLaser;
+import agency.shitcoding.arena.laser.GuardianLaser;
 import agency.shitcoding.arena.models.Ammo;
 import agency.shitcoding.arena.models.Weapon;
 import org.bukkit.*;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -42,11 +43,10 @@ public class LightningGunListener implements Listener {
     if (itemInMainHand.getType().isAir()
         || itemInMainHand.getType() != LIGHTNING
         || player.getCooldown(LIGHTNING) > 0) {
-        return;
+      return;
     }
 
     Weapon.applyCooldown(player, Weapon.LIGHTNING_GUN.cooldown);
-
 
     fireGun(player);
     Bukkit.getScheduler().runTaskLater(ArenaShooter.getInstance(), () -> fireGun(player), 1);
@@ -59,7 +59,8 @@ public class LightningGunListener implements Listener {
     if (Ammo.getAmmoForPlayer(player, Ammo.LIGHTNING) <= 0) {
       return;
     }
-    new AmmoUpdateEvent(player, -Weapon.LIGHTNING_GUN.ammoPerShot, Weapon.LIGHTNING_GUN.ammo).fire();
+    new AmmoUpdateEvent(player, -Weapon.LIGHTNING_GUN.ammoPerShot,
+        Weapon.LIGHTNING_GUN.ammo).fire();
 
     var name = player.getName();
     lastFired.put(name, System.currentTimeMillis());
@@ -72,55 +73,69 @@ public class LightningGunListener implements Listener {
     LivingEntity target = los.subList(1, los.size() - 1).stream()
         .map(block -> world.getNearbyLivingEntities(block.getLocation(), 1, 1, 1))
         .filter(foundEntities -> !foundEntities.isEmpty()
-            && foundEntities.stream().anyMatch(entity -> !entity.getName().equals(player.getName())))
+            && foundEntities.stream()
+            .anyMatch(entity -> validLaserTarget(entity, player)))
         .findFirst()
-        .map(foundEntities -> foundEntities.stream().findFirst().get()).orElse(null);
+        .map(foundEntities -> foundEntities.stream()
+            .filter(livingEntity -> validLaserTarget(livingEntity, player))
+            .findFirst().get()).orElse(null);
 
-    var finalLos = los.get(los.size() - 1);
+    var finalLos = los.getLast();
     var end = target != null ? target.getEyeLocation() : finalLos.getLocation();
     GuardianLaser laser = lasers.computeIfAbsent(name, s -> {
-      try {
-        GuardianLaser l = target != null
-            ? new GuardianLaser(eyeLocation, target, -1, 64)
-            : new GuardianLaser(eyeLocation, end, -1, 64);
-        Bukkit.getScheduler().runTaskTimer(ArenaShooter.getInstance(), task -> {
-          var lf = lastFired.get(name);
-          if (lf == null || System.currentTimeMillis() - lf > 300) {
-            l.stop();
-            lasers.remove(name);
-            lastFired.remove(name);
-            task.cancel();
-          }
-        }, 0, 1);
 
-        AtomicBoolean fullSecond = new AtomicBoolean(true);
-        Bukkit.getScheduler().runTaskTimer(ArenaShooter.getInstance(), task -> {
-          var lf = lastFired.get(name);
-          if (lf == null || System.currentTimeMillis() - lf > 300) {
-            task.cancel();
-          }
-          if (fullSecond.getAndSet(!fullSecond.get())) {
-            world.playSound(eyeLocation, SoundConstants.LG_FIRE, SoundCategory.VOICE, .75f, 1f);
-          }
-        }, 0, 9);
-        l.start(ArenaShooter.getInstance());
-        return l;
-      } catch (ReflectiveOperationException e) {
-        throw new RuntimeException(e);
-      }
+      GuardianLaser l = target != null
+          ? new GuardianLaser(eyeLocation, target)
+          : new GuardianLaser(eyeLocation, end);
+
+      Bukkit.getScheduler().runTaskTimer(ArenaShooter.getInstance(), task -> {
+        var lf = lastFired.get(name);
+        if (lf == null || System.currentTimeMillis() - lf > 300) {
+          l.stopBeam();
+          l.close();
+          lasers.remove(name);
+          lastFired.remove(name);
+          task.cancel();
+        }
+      }, 0, 1);
+
+      AtomicBoolean fullSecond = new AtomicBoolean(true);
+      Bukkit.getScheduler().runTaskTimer(ArenaShooter.getInstance(), task -> {
+        var lf = lastFired.get(name);
+        if (lf == null || System.currentTimeMillis() - lf > 300) {
+          task.cancel();
+        }
+        if (fullSecond.getAndSet(!fullSecond.get())) {
+          world.playSound(eyeLocation, SoundConstants.LG_FIRE, SoundCategory.VOICE, .75f, 1f);
+        }
+      }, 0, 9);
+      l.startBeam();
+      return l;
+
     });
 
-    try {
-      laser.moveStart(eyeLocation);
-      laser.moveEnd(target != null ? target.getEyeLocation() : end);
-    } catch (ReflectiveOperationException e) {
-      throw new RuntimeException(e);
-    }
+    /* Should be smooth as butter */
+    laser.setStart(eyeLocation);
+    laser.setTarget(target != null ? target.getEyeLocation() : end);
 
     if (target != null) {
       var originalVelocity = player.getVelocity();
-      new GameDamageEvent(player, target, GameplayConstants.LIGHTNING_DAMAGE, Weapon.LIGHTNING_GUN).fire();
-      Bukkit.getScheduler().runTaskLater(ArenaShooter.getInstance(), () -> player.setVelocity(originalVelocity), 1);
+      new GameDamageEvent(player, target, GameplayConstants.LIGHTNING_DAMAGE,
+          Weapon.LIGHTNING_GUN).fire();
+      Bukkit.getScheduler()
+          .runTaskLater(ArenaShooter.getInstance(), () -> player.setVelocity(originalVelocity), 1);
     }
+  }
+
+  private boolean validLaserTarget(LivingEntity entity, Player shooter) {
+    if (entity.getType() == EntityType.SQUID && entity.isInvisible()) {
+      return false;
+    }
+    if (entity.getType() == EntityType.GUARDIAN && entity.isInvisible()) {
+      return false;
+    }
+
+    return entity.getType() != EntityType.PLAYER || !entity.getUniqueId()
+        .equals(shooter.getUniqueId());
   }
 }
