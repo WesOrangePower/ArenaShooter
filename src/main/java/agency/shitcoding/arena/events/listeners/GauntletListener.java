71b424aac9abb7deef34e5c1b31d69a2b41c957f
Martin Borissov <admin@tooizi.eu>
2024-12-22 05:03:35 +0200

    Added Gauntlet gore

--- src/main/java/agency/shitcoding/arena/events/listeners/GauntletListener.java
+++ src/main/java/agency/shitcoding/arena/events/listeners/GauntletListener.java
@@ -1,9 +1,15 @@
 package agency.shitcoding.arena.events.listeners;
 
+import static agency.shitcoding.arena.GameplayConstants.GAUNTLET_DAMAGE;
+
 import agency.shitcoding.arena.events.GameDamageEvent;
 import agency.shitcoding.arena.events.GameShootEvent;
 import agency.shitcoding.arena.models.Weapon;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
 import org.bukkit.*;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -11,15 +17,12 @@ import org.bukkit.event.Listener;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.util.Vector;
 
-import java.util.HashSet;
-import java.util.Set;
-
-import static agency.shitcoding.arena.GameplayConstants.GAUNTLET_DAMAGE;
-
-
 public class GauntletListener implements Listener {
 
   private static final Material GAUNTLET = Weapon.GAUNTLET.item;
+
+  public static boolean bloodParticles = true;
+  private final Random random = new Random();
 
   @EventHandler
   public void onPlayerInteract(GameShootEvent event) {
@@ -40,17 +43,38 @@ public class GauntletListener implements Listener {
     world.spawnParticle(Particle.ELECTRIC_SPARK, at, 1, 0, 0, 0, 0);
 
     Set<LivingEntity> affectedEntities = new HashSet<>();
-    at.getWorld().getNearbyEntities(at, 1, 1, 1)
-        .stream()
+    at.getWorld().getNearbyEntities(at, 1, 1, 1).stream()
         .filter(LivingEntity.class::isInstance)
         .filter(entity -> entity != player)
-        .forEach(entity -> {
-          affectedEntities.add((LivingEntity) entity);
-          world.spawnParticle(Particle.ELECTRIC_SPARK, at, 1, 0, 0, 0);
-          world.playSound(at, Sound.ENTITY_GUARDIAN_ATTACK, 1f, 1f);
-        });
+        .forEach(
+            entity -> {
+              affectedEntities.add((LivingEntity) entity);
+              world.spawnParticle(Particle.ELECTRIC_SPARK, at, 1, 0, 0, 0);
+              world.playSound(at, Sound.ENTITY_GUARDIAN_ATTACK, 1f, 1f);
+              if (bloodParticles) {
+                sprayBlood(at, entity);
+              }
+            });
     affectedEntities.forEach(
-        entity -> new GameDamageEvent(player, entity, GAUNTLET_DAMAGE, Weapon.GAUNTLET)
-            .fire());
+        entity -> new GameDamageEvent(player, entity, GAUNTLET_DAMAGE, Weapon.GAUNTLET).fire());
+  }
+
+  private void sprayBlood(Location at, Entity entity) {
+    var entityCenter = entity.getLocation().add(0, entity.getHeight() / 2, 0);
+    var direction = entityCenter.toVector().subtract(at.toVector()).normalize();
+    var spray = direction.clone().multiply(0.5);
+    for (int i = 0; i < 10; i++) {
+      var offset = spray.clone().multiply(i);
+      at.getWorld()
+          .spawnParticle(
+              Particle.REDSTONE,
+              at.clone().add(offset),
+              1,
+              random.nextDouble(.5),
+              random.nextDouble(.5),
+              random.nextDouble(.5),
+              0,
+              new Particle.DustOptions(Color.fromRGB(0x770000), 2f));
+    }
   }
 }
